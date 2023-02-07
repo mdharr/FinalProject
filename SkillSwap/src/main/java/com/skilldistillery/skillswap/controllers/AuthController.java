53e8492680d47c78179fe8e1d791b1a724132956
Jay-tray <113144309+Jay-tray@users.noreply.github.com>
2023-02-07 12:06:28 -0700

    build out authContoller and green postman test on admin authentication

--- SkillSwap/src/main/java/com/skilldistillery/skillswap/controllers/AuthController.java
+++ SkillSwap/src/main/java/com/skilldistillery/skillswap/controllers/AuthController.java
@@ -1,14 +1,43 @@
 package com.skilldistillery.skillswap.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import java.security.Principal;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.skillswap.entities.User;
 import com.skilldistillery.skillswap.services.AuthService;
 
-@Controller
+@RestController
 @CrossOrigin({"*", "http://localhost"})
 public class AuthController {
   @Autowired
   private AuthService authService;
+  
+	@PostMapping("register")
+	public User register(@RequestBody User user, HttpServletResponse res) {
+	  if (user == null) {
+	     res.setStatus(400);
+	     return null;
+	  }
+	  user = authService.register(user);
+	  return user;
+	}
+	 
+	@GetMapping("authenticate")
+	public User authenticate(Principal principal, HttpServletResponse res) {
+	  if (principal == null) { // no Authorization header sent
+	     res.setStatus(401);
+	     res.setHeader("WWW-Authenticate", "Basic");
+	     return null;
+	  }
+	  return authService.getUserByUsername(principal.getName());
+	}
+
 }
--- SkillSwap/src/main/java/com/skilldistillery/skillswap/services/AuthServiceImpl.java
+++ SkillSwap/src/main/java/com/skilldistillery/skillswap/services/AuthServiceImpl.java
@@ -19,9 +19,9 @@ public class AuthServiceImpl implements AuthService {
 
 	@Override
 	public User register(User user) {
-//		user.setPassword(encoder.encode(user.getPassword()));
-//		user.setEnabled(true);
-//		user.setRole("standard");
+		user.setPassword(encoder.encode(user.getPassword()));
+		user.setEnabled(true);
+		user.setRole("standard");
 		userRepo.saveAndFlush(user);
 		return user;
 	}
