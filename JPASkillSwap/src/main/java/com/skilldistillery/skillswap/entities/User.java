68a15bac64929030c0443a8d274b6938fbafd134
Michael Harrington <mdharr7@gmail.com>
2023-02-08 01:39:31 +0800

    Stub JPA and Boot/REST projects

---
+++ JPASkillSwap/src/main/java/com/skilldistillery/skillswap/entities/User.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.skillswap.entities;
+
+public class User {
+
+}
---
+++ SkillSwap/src/main/java/com/skilldistillery/skillswap/SkillSwapApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skillswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSwapApplication.class, args);
+	}
+
+}
