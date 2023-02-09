2f98d4a738aa740d784f2442d6194d7297e1b869
Michael Harrington <mdharr7@gmail.com>
2023-02-10 02:03:14 +0800

    Complete project-member model

--- ngSkillSwap/src/app/models/project-member.ts
+++ ngSkillSwap/src/app/models/project-member.ts
@@ -1,2 +1,24 @@
+import { Project } from "./project";
+import { User } from "./user";
+
 export class ProjectMember {
+
+  // project member id might need a field
+  // id: number;
+  user: User;
+  project: Project;
+  comments: string;
+  rating: number;
+
+  constructor(
+    user: User,
+    project: Project,
+    comments: string = '',
+    rating: number = 0
+  ) {
+    this.user = user;
+    this.project = project;
+    this.comments = comments;
+    this.rating = rating;
+  }
 }
