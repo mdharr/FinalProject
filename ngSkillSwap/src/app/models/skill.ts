7c909726235e54a10295cac354afacba2e5dcfe6
Ryan Goode <goode21@gmail.com>
2023-02-09 10:53:19 -0700

    Build User and Address models

--- ngSkillSwap/src/app/models/skill.ts
+++ ngSkillSwap/src/app/models/skill.ts
@@ -1,2 +1,18 @@
 export class Skill {
+  id: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    description: string = '',
+    imageUrl: string = ''
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
 }
