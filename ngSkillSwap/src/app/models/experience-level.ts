4b3fa75409cad834725df758a7ba38ca22b9c18f
Michael Harrington <mdharr7@gmail.com>
2023-02-10 01:48:17 +0800

    Complete experience-level.ts model

--- ngSkillSwap/src/app/models/experience-level.ts
+++ ngSkillSwap/src/app/models/experience-level.ts
@@ -1,2 +1,14 @@
 export class ExperienceLevel {
+
+  id: number;
+  name: string;
+
+  constructor(
+    id: number = 0,
+    name: string = ''
+  ) {
+    this.id = id;
+    this.name = name;
+  }
+
 }
