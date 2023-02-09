ef56f6a25d540163a701d844335486cbf4037d83
laurenackley <lackley127@gmail.com>
2023-02-09 15:29:37 -0600

    Able to view all projects on home page, changed to where it doesn't auto login, changed text color

--- ngSkillSwap/src/app/components/home/home.component.ts
+++ ngSkillSwap/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from 'src/app/services/home.service';
+import { Project } from 'src/app/models/project';
 
 @Component({
   selector: 'app-home',
@@ -7,23 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-constructor(private auth: AuthService){}
-
+constructor(private auth: AuthService, private homeServ: HomeService){}
+ projects: Project[] = [];
 
 ngOnInit(){
-  this.testTestDeleteLater();
+  // this.testTestDeleteLater();
+  this.reload();
 }
 
-testTestDeleteLater(){
-  this.auth.login('admin', 'wombat1').subscribe({
-    next: (data) => {
-      console.log('logged in')
-      console.log(data);
-  },
+// testTestDeleteLater(){
+//   this.auth.login('admin', 'wombat1').subscribe({
+//     next: (data) => {
+//       console.log('logged in')
+//       console.log(data);
+//   },
+//     error:(fail) => {
+//       console.error('Error authenticating:')
+//       console.error(fail);
+//     }
+//   });
+//}
+reload(): void{
+  this.homeServ.index().subscribe({
+    next: (projects) =>{
+      this.projects = projects;
+    },
     error:(fail) => {
-      console.error('Error authenticating:')
+      console.error('Error getting projects:')
       console.error(fail);
     }
   });
 }
+
 }
---
+++ ngSkillSwap/src/app/services/home.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Project } from '../models/project';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+  private url = environment.baseUrl + 'api/projects';
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+  index(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.url, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(
+              'TodoService.index() error retrieving list of todos' + err
+            )
+        );
+      })
+    );
+  }
+}
