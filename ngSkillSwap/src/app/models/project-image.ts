944cfebf2edc2fdd9df56522bced451fdab5f2cd
Michael Harrington <mdharr7@gmail.com>
2023-02-10 01:55:31 +0800

    Complete model for project-image

--- ngSkillSwap/src/app/models/project-image.ts
+++ ngSkillSwap/src/app/models/project-image.ts
@@ -1,2 +1,24 @@
+import { Project } from "./project";
+
 export class ProjectImage {
+
+  id: number;
+  imageUrl: string;
+  caption: string;
+  createdDate: string;
+  project: Project;
+
+  constructor(
+    id: number = 0,
+    imageUrl: string = '',
+    caption: string = '',
+    createdDate: string = '',
+    project: Project
+  ) {
+    this.id = id;
+    this.imageUrl = imageUrl;
+    this.caption = caption;
+    this.createdDate = createdDate;
+    this.project = project;
+  }
 }
