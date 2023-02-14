10111a118744797010ffe39554482a7f5b6808bf
laurenackley <lackley127@gmail.com>
2023-02-13 18:37:00 -0600

    When account is registered, they are auto set to available

--- SkillSwap/src/main/java/com/skilldistillery/skillswap/services/AuthServiceImpl.java
+++ SkillSwap/src/main/java/com/skilldistillery/skillswap/services/AuthServiceImpl.java
@@ -21,6 +21,7 @@ public class AuthServiceImpl implements AuthService {
 	public User register(User user) {
 		user.setPassword(encoder.encode(user.getPassword()));
 		user.setEnabled(true);
+		user.setAvailability(true);
 		user.setRole("standard");
 		userRepo.saveAndFlush(user);
 		return user;
