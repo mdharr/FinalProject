25f2dfbda932bc0b7fd471cc0d8c4e9cb559bcc8
Michael Harrington <mdharr7@gmail.com>
2023-02-21 09:42:46 +0800

    Remove unnecessary code from profile component

--- ngSkillSwap/src/app/components/profile/profile.component.ts
+++ ngSkillSwap/src/app/components/profile/profile.component.ts
@@ -57,19 +57,6 @@ export class ProfileComponent implements OnInit {
     return options;
   }
 
-//   getCompletedProjects(){
-//     this.projectService.completedProjects().subscribe({
-//       next: (projects) => {
-//         this.completedProjects = projects;
-//       },
-//       error: (err) => {
-//         console.error(err);
-//         console.error('error retrieving completed projects: ' + err);
-//       }
-//   })
-// }
-
-
   ngOnInit(): void {
     this.authService.getLoggedInUser().subscribe({
       next: (user) => {
